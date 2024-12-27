405e5e3a0271d01d713bdbf90bdc4cded5e1d437
Francesco_Rubino <frarubino00@gmail.com>
2024-12-27 12:56:25 +0100

    migliorato il codice 'elab_generali.py'e aggiunto un nuovo file per l'estrazione dei fogli al 50.000

--- elab_generali.py
+++ elab_generali.py
@@ -1,17 +1,28 @@
 
 import geopandas as gpd
+import pandas as pd
 
-dataf = gpd.read_file ("C:\\Users\\fraru\\OneDrive\\Desktop\\UNIVERSITA'\\magistrale\\ICAR_Qgis\\Materiali\\Prodotto_da_me\\Carta_Uso_del_suolo_Taranto\\CDS_Taranto_2006\\CDS_Taranto_2006_ritagliato.shp")
+dataf = gpd.read_file ("C:\\Users\\fraru\\OneDrive\\Desktop\\Qgis\\cartine shapefile e QGIS\\cartina dell'Italia x comuni\\Com2011_WGS84\\Com2011_WGS84.shp")
 
-corrisp_dict = {
-    1: "superfici artificiali",
-    2: "superfici agricole utilizzate",
-    3: "superfici boscate ed altri ambienti naturali",
-    4: "ambiente umido",
-    5: "ambiente delle acque",
-    9: "superfici non classificate"
-}
+print ("colonne del dataframe")
+print (dataf.columns)
 
-dataf ["desc_L1"] = dataf ["L1_2006"].map (corrisp_dict)
-print (dataf [["L1_2006" , "desc_L1"]])
+dataf_Puglia = dataf [dataf ["COD_REG"] == 16]
+print ("comuni della Puglia")
+print (dataf_Puglia)
+
+lista_comuni_16 = list (dataf_Puglia ["NOME"])
+
+print ("elenco comuni della Puglia:")
+print (lista_comuni_16)
+
+print ("numero comuni della Puglia:")
+print (len (lista_comuni_16))
+
+dataf_comuni = pd.read_csv ("C:\\Users\\fraru\\OneDrive\\Desktop\\Qgis\\cartine shapefile e QGIS\\cartina dell'Italia x comuni\\Com2011_WGS84\\Elenco_comuni_Italiani_ISTAT.csv" , sep =";")
+print (dataf_comuni)
+
+print (dataf_comuni.columns)
+
+
 
